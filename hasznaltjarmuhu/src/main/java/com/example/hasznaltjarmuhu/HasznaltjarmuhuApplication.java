0e3a91e577458480f2ec2dbe3c938b1e55c2a093
MizereRobin <mizererobin@gmail.com>
2025-01-16 11:51:40 +0100

    alapok

---
+++ hasznaltjarmuhu/src/main/java/com/example/hasznaltjarmuhu/HasznaltjarmuhuApplication.java
@@ -0,0 +1,13 @@
+package com.example.hasznaltjarmuhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasznaltjarmuhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasznaltjarmuhuApplication.class, args);
+	}
+
+}
